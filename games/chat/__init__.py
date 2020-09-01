4168e47a32f2538eecbc43057eeb15c53fef61a6
Sawatdekrap <sawatdekrap@gmail.com>
2020-09-01 21:20:07 +1000

    Squashed commit of the following:
    commit caa14deece4581e5bc578f79396482065ef145c2
    Author: Sawatdekrap <sawatdekrap@gmail.com>
    Date:   Tue Aug 25 22:47:59 2020 +1000
    
        Some cleanup
    
    commit f3cd89035d37ca428825dcb86077b6d831ab966d
    Author: Sawatdekrap <sawatdekrap@gmail.com>
    Date:   Tue Aug 25 22:43:11 2020 +1000
    
        Change stages and their handling to be more managable. Remove all CLRF from files...

--- games/chat/__init__.py
+++ games/chat/__init__.py
@@ -10,19 +10,28 @@ class ChatUser(BaseUser):
 
 
 class ChatStage(BaseStage):
-    async def handle_event(self, event, game):
-        logger.info(f"New event: {event.user}: {event.action}")
-        await game.host.q.put(f"{event.user}: {event.action}")
+    def handle_event(self, event, game):
+        return True
+
+    async def post_handle_event(self, event, game, handle_obj):
+        if event.user is None:
+            logger.info("tick")
+        else:
+            logger.info(f"New event: {event.user}: {event.action}")
+            await game.host.q.put(f"{event.user}: {event.action}")
+
+    def is_complete(self):
+        return False
 
 
 class Chat(BaseGame):
     USER_CLASS = ChatUser
 
     def build_stage_list(self):
-        return [ChatStage(timeout=None)]
+        return [ChatStage()]
 
     async def post_register_user(self, user):
         await self.host.q.put(f"User {user} has joined")
 
     async def pre_remove_user(self, user):
-        await self.host.q.put(f"User {user} has left")           
+        await self.host.q.put(f"User {user} has left")
