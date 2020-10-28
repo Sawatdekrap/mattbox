2f9bf252d3d2c41885a71b7e1bf5a662192f2b20
Sawatdekrap <sawatdekrap@gmail.com>
2020-10-28 23:07:10 +1100

    Some additions to server and game structure

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,68 @@
+import { textSpanIntersectsWith, updateImportEqualsDeclaration } from 'typescript';
 import * as WebSocket from 'ws';
 import { Messages } from '../common/constants';
 
-const wss = new WebSocket.Server({
-  port: 8080
-});
+interface GameItf {
+  capacity: number;
+};
 
-wss.on('connection', (sock) => {
-  sock.on('message', (msg) => {
-    console.log(msg);
-    sock.send(msg);
-  });
+type UUID = string;
 
-  sock.send(Messages.CONNECTED);
-});
+interface UserProps {
+  uuid: UUID;
+  sock: WebSocket;
+};
+
+class User {
+  uuid: UUID;
+  sock: WebSocket;
+  game: GameItf;
+
+  constructor(props: UserProps, game: GameItf) {
+    this.uuid = props.uuid;
+    this.sock = props.sock;
+    this.game = game;
+
+    this.registerEvents();
+  };
+
+  private onMessage(message: string){
+    //
+  };
+
+  registerEvents() {
+    this.sock.on('message', this.onMessage);
+  };
+};
+
+// TODO move users to game?
+class Server {
+  users: Record<UUID, User>;
+  game: GameItf;
+  serverProps: WebSocket.ServerOptions;
+
+  constructor(game: GameItf, serverProps: WebSocket.ServerOptions) {
+    this.users = {};
+    this.game = game;
+    this.serverProps = serverProps;
+  };
+
+  private onConnection(sock: WebSocket) {
+    const user = new User({
+      uuid: (Math.random() * 20).toString(),
+      sock: sock,
+    }, this.game);
+
+    if (Object.keys(this.users).length < this.game.capacity) {
+      this.users[user.uuid] = user;
+      sock.on('close', () => {
+        delete this.users[user.uuid];
+      });
+    }
+  };
+
+  run() {
+    const wss = new WebSocket.Server(this.serverProps);
+    wss.on('connection', this.onConnection);
+  };
+};
