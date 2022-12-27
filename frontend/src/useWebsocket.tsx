bbb697064f07e3ec3f40f6e37ac01240f2613f78
sawatdekrap <sawatdekrap@gmail.com>
2022-12-27 16:48:10 +1100

    Fix multiple websocket connections on render

--- frontend/src/useWebsocket.tsx
+++ frontend/src/useWebsocket.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { ComponentItf } from "./interfaces";
 
 interface UseWebsocketProps {
@@ -12,31 +12,41 @@ const useWebsocket = ({
   onComponentUpdate,
   onLayoutUpdate,
 }: UseWebsocketProps) => {
-  const url = `ws://localhost:8000/games/${gameId}`;
+  const ws = useRef<WebSocket>();
 
-  const ws = new WebSocket(url);
+  useEffect(() => {
+    if (ws.current) return;
 
-  ws.addEventListener("open", () => {
-    console.log("Connection open");
-  });
-  ws.addEventListener("close", () => {
-    console.log("Connection closed");
-  });
-  ws.addEventListener("message", (e) => {
-    // TODO properly serialize/deserialize
-    const dataString: string = e.data.toString();
-    console.log(`Recieved: ${dataString}`);
+    const url = `ws://localhost:8000/games/${gameId}`;
+    ws.current = new WebSocket(url);
 
-    if (dataString.startsWith("[")) {
-      const components = JSON.parse(dataString) as ComponentItf[];
-      onLayoutUpdate(components);
-    } else {
-      onComponentUpdate(dataString);
-    }
-  });
+    ws.current.addEventListener("open", () => {
+      console.log("Connection open");
+    });
+    ws.current.addEventListener("close", () => {
+      console.log("Connection closed");
+    });
+    ws.current.addEventListener("message", (e) => {
+      // TODO properly serialize/deserialize
+      const dataString: string = e.data.toString();
+      console.log(`Recieved: ${dataString}`);
+
+      if (dataString.startsWith("[")) {
+        const components = JSON.parse(dataString) as ComponentItf[];
+        onLayoutUpdate(components);
+      } else {
+        onComponentUpdate(dataString);
+      }
+    });
+
+    return () => {
+      console.log("Exiting useWebsocket render");
+      ws.current && ws.current.close();
+    };
+  }, [gameId]);
 
   const sendComponentUpdate = (data: string) => {
-    ws.send(data);
+    ws.current && ws.current.send(data);
   };
 
   return { sendComponentUpdate };
